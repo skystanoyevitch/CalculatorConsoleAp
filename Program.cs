c3113d1501e36ed0b0409e7d8a18bd7dc0b32a30
Sky <skystanoyevitch@proton.me>
2024-07-17 11:01:43 -0700

    Added new class library + project reference for CalculatorLibrary Class

--- Program.cs
+++ Program.cs
@@ -1,37 +1,7 @@
 ﻿using System.Text.RegularExpressions;
+namespace CalculatorProgram;
 
-class Calculator
-{
-    public static double DoOperation(double num1, double num2, string op)
-    {
-        double result = double.NaN;
 
-        switch (op)
-        {
-            case "a":
-                result = num1 + num2;
-                break;
-            case "-":
-                result = num1 - num2;
-                break;
-            case "*":
-                result = num1 * num2;
-                break;
-            case "d":
-
-                if (num2 != 0)
-                {
-                    result = num1 / num2;
-                }
-                break;
-            default:
-                break;
-        }
-
-        return result;
-
-    }
-}
 
 
 class Program
